f2b78e8d62600d5447ba64bceaad2bb2420ef13a
Kamil Kreli <kamil.kreli@gmail.com>
2021-04-18 19:38:17 +0200

    Added Text Mesh Pro tweens

---
+++ Assets/EasyTween/Runtime/Tweens/Alpha/TextMeshProAlphaTween.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace EasyTween
+{
+    public sealed class TextMeshProAlphaTween : BaseTween
+    {
+        readonly TMP_Text target;
+        readonly float value;
+
+        float startValue;
+        float endValue;
+
+        public TextMeshProAlphaTween(TMP_Text target, float value) : base()
+        {
+            this.target = target;
+            this.value = value;
+        }
+
+        internal override void Initialize()
+        {
+            startValue = target.color.a;
+            endValue = value;
+        }
+
+        internal override void Lerp(float ratio)
+        {
+            Color newColor = target.color;
+            newColor.a = Mathf.LerpUnclamped(startValue, endValue, ratio);
+            target.color = newColor;
+        }
+    }
+}
---
+++ Assets/EasyTween/Runtime/Tweens/Color/TextMeshProColorTween.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace EasyTween
+{
+    public sealed class TextMeshProColorTween : BaseTween
+    {
+        readonly TMP_Text target;
+        readonly Color value;
+
+        Color startValue;
+        Color endValue;
+
+        public TextMeshProColorTween(TMP_Text target, Color value) : base()
+        {
+            this.target = target;
+            this.value = value;
+        }
+
+        internal override void Initialize()
+        {
+            startValue = target.color;
+            endValue = value;
+        }
+
+        internal override void Lerp(float ratio)
+        {
+            target.color = Color.LerpUnclamped(startValue, endValue, ratio);
+        }
+    }
+}
